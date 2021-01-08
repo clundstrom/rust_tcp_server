0dbac8124a6ed3971a3435acbe7919dd45b8152d
Christoffer Lundström <lulleh_@hotmail.com>
2021-01-08 18:42:41 +0100

    added argument sanitizing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 mod network_layer;
-use std::env;
+
+//use log::{warn};
+use std;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
+    sanitize_args(&args);
     // Insert argument values here
-    let network = network_layer::AbstractNetworkLayer;
-    network.sanitize_args(&args);
-    network.schedule_task("Test")
+    let network = network_layer::AbstractNetworkLayer::new(1, 30, 8000, 1);
 
+    network.schedule_task("Test")
+}
+
+/// Sanitizes application arguments to meet requirements
+///
+/// Requirements: Port, buff size.
+pub fn sanitize_args(args: &Vec<String>) -> (i32, i32) {
+    let mut res = vec![];
+
+    for i in 1..args.len() {
+        let split: Vec<&str> = args[i].split("=").collect();
+
+        let result = split[1]
+            .trim()
+            .parse()
+            .expect("Failed to parse argument. Not an integer.");
+
+        res.insert(res.len(), result);
+    }
+    println!("{} {}", res[0], res[1]);
+    (res[0], res[1])
 }
--- src/network_layer.rs
+++ src/network_layer.rs
@@ -1,17 +1,18 @@
 use log::{warn};
-use std::collections::{HashMap};
 
 pub struct AbstractNetworkLayer {
     transfer_rate: i32,
     bufsize: i32,
-    early_terminate: bool,
+    early_terminate: u8,
     port: u16, // 0-65535
 }
 
 trait NetworkLayer {}
 
 impl AbstractNetworkLayer {
-    pub fn new(transfer_rate: i32, bufsize: i32, port: u16, early_terminate: bool) -> AbstractNetworkLayer {
+
+    // Pseudo-constructor
+    pub fn new(transfer_rate: i32, bufsize: i32, port: u16, early_terminate: u8) -> AbstractNetworkLayer {
         AbstractNetworkLayer { transfer_rate, bufsize, early_terminate, port }
     }
 
@@ -20,25 +21,5 @@ impl AbstractNetworkLayer {
     ///
     pub fn schedule_task(&self, message: &str) -> () {
         println!("{}", &message);
-    }
-
-
-    ///
-    /// Sanitizes application arguments to meet requirements
-    ///
-    /// Requirements: Port, buff size.
-    ///
-    pub fn sanitize_args(&self, args: &Vec<String>) {
-        //let mut valid = HashMap::new();
-
-        for i in 0..args.len() {
-            let mut values: Vec<&str> = args[i].split(" ").collect();
-
-            println!("Key: {}", args[i])
-        }
-
-        if args.len() < 1 {
-            warn!("Using default port 8000");
-        }
     }
 }
